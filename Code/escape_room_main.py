30d16f1c48bd072dc0ba0636bda79797448b96ae
Ole Faber <ole.faber@outlook.de>
2024-11-09 16:22:50 +0100

    added background for start window

--- Code/escape_room_main.py
+++ Code/escape_room_main.py
@@ -3,27 +3,27 @@
 import tkinter as tk
 import Zelle
 
-# Hauptfenster erstellen
+# Startfenster erstellen
 root = tk.Tk()
 root.title("Prison-Escape")
 root.geometry("800x800")
 root.resizable(width=False, height=False)
 
-#Starttext
-text = tk.Label(root, text="Prison-Escape",  font='Helvetica 26 bold')
-text.place(relx = 0.5, rely = 0.4, anchor = 'center')
+#Startfenster Canvas erstellen
+canvas = tk.Canvas(root, width=800, height=800, bg="black")
+canvas.pack()
 
-#Startfenster Hintergrund
-#canvas = tk.Canvas(root, width=800, height=800, bg="black")
-#canvas.pack()
-#prisonImg = tk.PhotoImage(file="")
-#prisonBg = canvas.create_image(0,0,image=prisonImg, anchor='nw')
+Img = tk.PhotoImage(file="../images/prison.png")
+#text = tk.Text(text="Prison-Escape",  font='Helvetica 32 bold', fg="white")
+
+prisonBg = canvas.create_image(0,0,image=Img, anchor='nw')
+#prisonText = canvas.create_text(0,0,text=text, anchor='nw', fill="white")
 
 #Button -> Geht zum ersten Raum
 def start_cell():
     root.destroy()
     Zelle.run()
-raum1 = tk.Button(root, text="Start", font='Helvetica 22', command=start_cell, height=2, width=12)
+raum1 = tk.Button(root, text="Start", font='Helvetica 22', command=start_cell, height=2, width=12, bg="black", fg="white")
 raum1.place(relx = 0.5, rely = 0.55, anchor = 'center')
 
 # GUI starten
