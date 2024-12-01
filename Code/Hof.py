51d6009633e8997eb778950bc76fdb6bc5b75785
Hubert <hu.blin@ostfalia.de>
2024-12-01 12:50:49 +0100

    Kommentare hinzugefügt sowie Falsche eingabe 3x probiert

--- Code/Hof.py
+++ Code/Hof.py
@@ -1,60 +1,65 @@
 #if uebergang == True:
-import time
+import time # Importieren des Zeit Moduls, um Pausen zwischen den print-Befehlen zu erzeugen
 
-code = {}
+code = {} # leeres Dictionary, um Codes zu speichern, die der Spieler während des Spiels sammelt
 
+# Diese Funktion simuliert die erste Stufe des Spiels, in der der Spieler Hinweise und einen Schlüssel finden muss,
+# um in den Wachturm zu gelangen.
 def gefaengnishof_1():
     print("Du hast in der naechsten Stufe den Gefaengnishof erreicht und musst einen Weg finden, in den Wachturm zu gelangen.")
-    time.sleep(5)
+    time.sleep(2)
     print("Als du ueber den Hof gehst, siehst du mehrere einzelne Stationen.")
-    time.sleep(5)
+    time.sleep(2)
     print("Du kommst auf dem Weg zum Wachturm an einer Treppe, einem Tisch und einer Kiste vorbei.")
-    time.sleep(5)
-    print("Der Wachturm ist verschlossen und man benoetigt einen Schluessel, um hineinzukommen.")
+    time.sleep(2)
+    print("Der Wachturm ist verschlossen und man benoetigt einen Schluessel, um hineinzukommen.") # Einleitung in das Thema des Raumes
 
-    schluessel_gefunden = False
-    teilcode_gefunden = False
-    teilcode2_gefunden = False
-    tuer_wachturm = False
+    #Variablen, die den Fortschritt des Spielers speichern
+    schluessel_gefunden = False # Gibt an, ob der Schlüssel gefunden wurde
+    teilcode_gefunden = False   # Gibt an, ob der erste Code gefunden wurde
+    teilcode2_gefunden = False  # Gibt an, ob der zweite Code gefunden wurde
+    tuer_wachturm = False       # Gibt an, ob die Wachturm-Tür bereits geöffnet wurde
 
+    # Schleife, damit alle Stationen wiederholt werden, bis Wachturm erreicht wurde
     while True:
         print("Du siehst dir nochmals die Umgebung an.")
-        kommando = input("Zu welcher Station moechtest du gehen? (Treppe/Kiste/Tisch/Wachturm): ").lower()
-        time.sleep(3)
+        kommando = input("Zu welcher Station moechtest du gehen? (Treppe/Kiste/Tisch/Wachturm): ").lower() # Spieler wählt Station
+        time.sleep(2)
 
-        if kommando == "treppe":  # Erster Teil zum entkommen
-            if not schluessel_gefunden:
+        # Überprüfen der Eingabe (Station)
+        if kommando == "treppe":
+            if not schluessel_gefunden: # Überprüfen, ob Schlüssel bereits gefunden
                 print("Glueckwunsch! Unter der Treppe liegt ein Schluessel,den ein Wachmann dort verloren haben musss.")
                 time.sleep(2)
                 print("Du hebst den Schluesssel auf")
-                schluessel_gefunden = True
-                time.sleep(3)
+                schluessel_gefunden = True # Variable wird auf True gesetzt, da Schlüssel gefunden wurde
+                time.sleep(2)
             else:
                 print("Die Treppe wurde bereits untersucht.")
                 time.sleep(2)
 
-        elif kommando == "kiste":  # Zweiter Teil (Hinweis 1)TREPPE
-            if not teilcode_gefunden:
+        elif kommando == "kiste":
+            if not teilcode_gefunden: # Überprüfen, ob Teilcode aus Kiste bereits gefunden
                 print("In der Kiste ist ein Hinweis mit einem Code: 2357.")
-                benutzer_code = input("Bitte notiere den Code, damit du ihn nicht vergisst: ")
-                if benutzer_code == "2357":  # ueberpruefen, ob der eingegebene Code richtig ist
+                benutzer_code = input("Bitte notiere den Code, damit du ihn nicht vergisst: ") # Aufforderung den Code zu notieren, für spätere Spielschritte
+                if benutzer_code == "2357":  # Überpruefen, ob der eingegebene Code richtig ist
                     code["CODE_Teil1"] = benutzer_code
-                    teilcode_gefunden = True
+                    teilcode_gefunden = True # Variable wird auf True gesetzt, wenn Teilcode "Kiste" gefunden
                     time.sleep(2)
                 else:
                     print("Falscher Code!")
                     time.sleep(2)
             else:
                 print("Die Kiste wurde bereits untersucht.")
-                time.sleep(3)
+                time.sleep(2)
 
-        elif kommando == "tisch":  # Dritter Teil (Hinweis 2)
-            if not teilcode2_gefunden:
+        elif kommando == "tisch":
+            if not teilcode2_gefunden: # Überprüfen, ob Teilcode vom Tisch bereits gefunden
                 print("Unter dem Tisch ist ein Hinweis mit einem Code: 7685 den ein Informant fuer dich dort hinterlassen hat")
-                benutzer_code = input("Bitte notiere den Code,damit du ihn nicht vergisst: ")
-                if benutzer_code == "7685":  # ueberpruefen, ob der eingegebene Code richtig ist
+                benutzer_code = input("Bitte notiere den Code,damit du ihn nicht vergisst: ") # Aufforderung den Code zu notieren, für spätere Spielschritte
+                if benutzer_code == "7685":  # Überpruefen, ob der eingegebene Code richtig ist
                     code["CODE_Teil2"] = benutzer_code
-                    teilcode2_gefunden = True
+                    teilcode2_gefunden = True # Variable wird auf True gesetzt, wenn Teilcode "Tisch" gefunden
                     time.sleep(2)
                 else:
                     print("Falscher Code!")
@@ -63,28 +68,30 @@ def gefaengnishof_1():
                 print("Die Bank wurde bereits untersucht.")
                 time.sleep(2)
 
-        elif kommando == "wachturm":  # Letzter Teil zum entkommen
-            if not tuer_wachturm:
+        elif kommando == "wachturm":
+            if not tuer_wachturm: # Überprüfen, ob Wachturm bereits betreten wurde
+                # Überprüfe, ob der Spieler alle notwendigen Hinweise gefunden hat, um in diese Station zu gelangen
                 if schluessel_gefunden and teilcode_gefunden and teilcode2_gefunden:
-                    print(
-                        "Du hast alle Hinweise in den Stationen sowie den Schluessel gefunden und kommst somit in die naechste Stufe.")
-                    #tuer_wachturm = True
-                    time.sleep(3)
-                    break
+                    print("Du hast alle Hinweise in den Stationen sowie den Schluessel gefunden und kommst somit in die naechste Stufe.")
+                    tuer_wachturm = True # Variable wird auf True gesetzt, wenn Wachturm betreten wird
+                    time.sleep(2)
+                    break # Schleife wird beendet und nächste Station wird erreicht
                 else:
                     print("Du musst erst alle Hinweise in dieser Stufe finden, um in den Wachturm zu gelangen")
-                    time.sleep(3)
+                    time.sleep(2)
             else:
                 print("Der Wachturm ist bereits betreten worden.")
                 time.sleep(2)
-        else:
+        else: # Fehlermeldungen für die Eingabe falscher Kommandos
             print("Unbekannter Befehl. Versuche eines der vier Befehle: Treppe, Kiste, Bank, Wachturm.")
             time.sleep(2)
-#Aufruf der Funktion
+
+# Aufruf der Funktion um die erste Station
 gefaengnishof_1()
 
 print("Herzlichen Glueckwunsch, du bist in der naechsten Stufe")
-
+# Diese Funktion simuliert die zweite Stufe des Spiels, in der der Spieler einen weiteren Code und ein Seil finden muss,
+# um aus dem Wachturm zu entkommen.
 def gefaengnishof_2_wachturm():
     print("Du hast nun den Wachturm erreicht und bist nun vor der letzten Stufe unseres Spiels")
     time.sleep(2)
@@ -94,54 +101,59 @@ def gefaengnishof_2_wachturm():
           "Ganz oben im Wachturm ist dir noch ein Fenster aufgefallen, welches nicht versperrt ist und aus dem Gefaengnis fuehrt")
     time.sleep(2)
     print("Du hast ohne Erfolg alle versperrten Tueren versucht mit dem gefundenen Schluessel aufzuschließen, \n"
-          "also ist deine letzte Hoffnung die Abstellkammper ")
+          "also ist deine letzte Hoffnung die Abstellkammper ") # Einleitung in den Wachturm und zur neuen Herausforderung
     time.sleep(2)
     print("Dein Informant, der die Hinweise sorgfaeltig versteckt hat, erklaerte, \n"
           "dass sich das Passwort aus dem einen Hinweis minus dem anderen Hinweis zusammensetzt. Er war sich aber unsicher wie herum")
-    print("Hinweis aus der ersten Stufe", code.get("CODE_Teil1"),",", code.get("CODE_Teil2"))
+    print("Hinweis aus der ersten Stufe", code.get("CODE_Teil1"),",", code.get("CODE_Teil2")) # Hinweise zum digitalen Zahlenschloss
 
-    anzahl_versuche = 3
-    while anzahl_versuche > 0:
-        passwort = input("Bitte geben Sie das Passwort ein:")
-        anzahl_versuche -= 1
+    # Eingabe des Codes
+    anzahl_versuche = 3 # Der Spieler hat drei Versuche das richtige Passwort einzugeben
+    while anzahl_versuche > 0: # Schleife die solange läuft, bis keine Versuche
+        passwort = input("Bitte geben Sie das Passwort ein:") # Spieler gibt Passwort ein
+        anzahl_versuche -= 1 # Reduziert verbleibenden Versuche um 1
 
-        if passwort == "5328":
+        if passwort == "5328": # Richtige Kombnation aus Code 1 - Code 2 (Differenz zwischen 7685 und 2357)
             print("Die Tuer laesst sich oeffnen")
-            break
-        else:
+            break # beendet Schleife da richtige Eingabe
+        else: # falsche Eingabe, informiert Spieler über verbleibende Versuche
             print("Passwort falsch")
             print("Sie haben noch", anzahl_versuche, "versuche")
-            if anzahl_versuche == 0:
-                print("Das Nummernfeld geht aus und Sie kommen nicht weiter") # hier wieder nach ganz vorne?!
-                # oder hier wieder an den anfang dieser Funktion
-    #neue etappe im Code
-    print("In der Abstellkammer findest du einen weiteren Schlüssel")
+            if anzahl_versuche == 0: # Wenn keine Versuche mehr übrig sind muss als Strafe der vorherige Raum gespielt werden, sodass man weiter kommt
+                print("Das Nummernfeld geht aus und Sie kommen nicht weiter")
+                time.sleep(2)
+                print("Du wirst eine Station nach hinten versetzt")
+                time.sleep(2)
+                gefaengnishof_1() # startet erste Funktion neu
+
+    print("In der Abstellkammer findest du einen weiteren Schlüssel") # Belohnung für das Öffnen der Tür
     time.sleep(2)
     print("Du testet ein weiteres mal mit diesem Schlüssel die Türen und es lässt sich eine Tür öffnen!")
     time.sleep(2)
     print("Der Raum ist dunkel und du siehst hinten in der Ecke nur einen Umriss von einem Schreibtisch. \n"
           "In der andern Ecke steht eine Truhe und rechts von dir steht eine kleiner Werkzeugkoffer")
     time.sleep(2)
-    print("Die Truhe ist wieder mit einem Zahlenschloss versperrt")
+    print("Die Truhe ist wieder mit einem Zahlenschloss versperrt") # neue Herausforderung
 
-    seil_gefunden = False
-    code_gefunden = False
+    # Variablen, die den Fortschritt des Spielers speichern
+    seil_gefunden = False # Gibt an, ob das Seil bereits gefunden wurde.
+    code_gefunden = False # Gibt an, ob der neue Code gefunden wurde.
 
-    while seil_gefunden == False:
+    while seil_gefunden == False: # Schleife, die läuft, bis das Seil gefunden wurde.
         kommando = input("Was möchtest du dir ansehen? Schreibtisch / Truhe / Werkzeugkoffer:").lower()
 
-        if kommando == "schreibtisch":
-            if code_gefunden == False:
+        if kommando == "schreibtisch": # Spieler untersucht den Schreibtisch.
+            if code_gefunden == False: # Code wurde noch nicht gefunden.
                 print("Auf dem Schreibtisch liegt nur ein Stift und ein Block")
                 time.sleep(2)
                 print("In der Schreibtischschublade ist ein kleines Notizbuch, indem auf der ersten Seite ein weiterer Code notiert ist")
                 time.sleep(2)
                 print("Der Code ist 1234")
-
+                # Spieler muss den Code korrekt eingeben.
                 benutzer_code = input("Bitte notiere den Code damit du ihn nicht vergisst:")
-                if benutzer_code == "1234":
-                    code_gefunden = True
-                    code["CODE_Teil3"] = benutzer_code
+                if benutzer_code == "1234": # Richtiger Code.
+                    code_gefunden = True # Speichert, dass der Code gefunden wurde.
+                    code["CODE_Teil3"] = benutzer_code # Fügt den Code zum Dictionary hinzu.
                     print("richtiger Code!")
                     time.sleep(2)
 
@@ -149,39 +161,40 @@ def gefaengnishof_2_wachturm():
                     print("Falscher Code!")
             else:
                 print("Der Schreibtisch wurde bereits untersucht")
-        elif kommando == "werkzeugkoffer":
+        elif kommando == "werkzeugkoffer": # Spieler untersucht den Werkzeugkoffer.
             print("Der Werkzeugkoffer ist leer")
-        elif kommando == "truhe":
+        elif kommando == "truhe": # Spieler untersucht die Truhe.
             if seil_gefunden == False:
-                if code_gefunden == True:
+                if code_gefunden == True: # Überprüft, ob der benötigte Code gefunden wurde.
                     print("Die Truhe ist wie schon erwäht mit einem Zahlenschloss verschlossen")
                     time.sleep(2)
                     print("Villeicht bringt dich der Code, den du gerade notiert hast, weiter", code.get("CODE_Teil3"))
-                    while True:
+                    # Spieler gibt den Code für die Truhe ein.
+                    while True: # Ausnahmebehandlung für den Valueerror
                         try:
                             zahlencode_truhe = int(input("Bitte gib den Code ein"))
-                            if zahlencode_truhe == 1234:
+                            if zahlencode_truhe == 1234: # richtiger Code
                                 print("richtiger Code!")
                                 time.sleep(2)
-                                break
+                                break # beendet Eingabeschleife, nachdem der code korrekr eingegeben wurde
                             else:
                                 print("Falscher Code!")
 
-                        except ValueError as e:
+                        except ValueError as e: # Fehlermeldung falls ien String eingegeben wurde
                             print("Sie dürfen keinen String eingeben", e)
-                    print("In der Truhe liegt ein Seil")
-                    seil_gefunden = True
+                    print("In der Truhe liegt ein Seil") # Belohnung für das Lösen des Rätsels.
+                    seil_gefunden = True  # Fortschritt wird aktualisiert.
 
                 else:
-                    print("Du musst erst einen anderen Hinweis finden")
+                    print("Du musst erst einen anderen Hinweis finden") # Code fehlt noch
             else:
                 print("Die Truhe wurde bereits geöffnet")
                 time.sleep(2)
-        else:
+        else: # Fehlermeldung bei falscher eingabe
             print("Unbekannter Befehl, probiere Schreibtisch / Truhe / Werkzeugkoffer")
     print("Mit dem Seil könntest du versuchen aus dem Fenster im oberen Stockwerk zu klettern")
     time.sleep(2)
-    print("oben angekommen öffnest du das Fenster, kletterst runter und flüchtest")
+    print("oben angekommen öffnest du das Fenster, kletterst runter und flüchtest") # Abschluss der Herausforderung und Hinweise zur Flucht
 
 #Aufruf der zweiten Funktion
 gefaengnishof_2_wachturm()
