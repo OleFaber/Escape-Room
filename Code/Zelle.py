f4782682b676354629297c4a38192b7ae810808b
Ole Faber <ole.faber@outlook.de>
2024-12-01 14:28:44 +0100

    Coding session - 01.12.24

--- Code/Zelle.py
+++ Code/Zelle.py
@@ -1,6 +1,5 @@
 #Erster Raum - Gefängniszelle
 import tkinter as tk
-import Waerterbuero
 
 def run():
     # Hauptfenster erstellen
@@ -9,36 +8,61 @@ def run():
     cell.geometry("800x800")
     cell.resizable(width=False, height=False)
 
-    # Funktion zur Überprüfung des Lösungsworts
+    # Funktion zur Überprüfung des Lösungsworts (Verarbeitung der Eingabe)
     def check_password():
-        if eingabe == "code":
-            output = "Glückwunsch! Die Tür öffnet sich."
-            Waerterbuero.run()
+        eingabe = user_input.get().lower()
+        if eingabe == "freiheit":
+            output = ("Glückwunsch! Die Tür öffnet sich.")
         else:
-            output = "Fehler!"
+            output = ("Falsches Passwort")
+        # Text zur Ausgabe hinzufügen
+        text_output.delete("1.0", tk.END)
+        text_output.insert("1.0", output)
 
-        # Ausgabefeld Schloss
-        label_output = tk.Label(cell, text=output)
-        label_output.pack(pady=10)
-
-    # Begrüßungstext
-    label_text = tk.Label(cell, text="Du erwachst in einer kalten, düsteren Gefängniszelle.\n" 
-                                      "Die Luft ist feucht, und nur ein schwacher Lichtstrahl fällt durch ein kleines Fenster hoch oben an der Wand.\n"
-                                      "An der Wand erkennst du eine Nachricht, die jemand eingeritzt hat:\n"
-                                      "\n\"Der Weg in die Freiheit ist nahe, wenn du die Zeichen erkennst. COuragiert und aufmerksam, "
-                                      "Damit du den SchlüssEl findest!\"\n", wraplength=400, justify="center")
-    label_text.pack(pady=10)
+    # Text Widget erstellen
+    text_widget = tk.Text(cell, wrap="word", bg="black", fg="white", font=("Helvetica", 12, 'bold'))
+    #Text Widget platzieren
+    text_widget.pack(pady=10, fill="both", expand=False)
+    # Textinhalt setzen
+    content = ("Langsam öffnest du deine Augen und Findest dich alleine in einer kalten, düsteRen Gefängniszelle wieder.\n"
+    "Die AtmosphärE ist erdrückend, und die massIven Steinwände verstärken das Gefühl der Einsamkeit.\n"
+    "Du bemerkst, dass jemand eine Botschaft in die kahle Betonwand eingeritzt hat:\n\n"
+    "\"In der Stunde der DunkelHeit liegt der Schlüssel zum Entkommen verborgen. Ein mutiger Schritt kann den ersten "
+    "StEin ins Rollen bringen. Öffne deine Augen und suche nach den verborgenen Hinweisen.\n"
+    "Vertraue auf deine Instinkte, und sei stets wachsam. Die Wahrheit liegt nicht immer offensichtlich vor dir. "
+    "Beginne mit einem tiefen ATemzug, lausche dem Klang der Stille und folge dem Flüstern der Hoffnung.\"\n\n"
+    "An der Zellentür findest du ein Eingabefeld, bei dem du ein Password eingeben musst.")
+    # Textinhalt dem Text-Widget hinzufügen
+    text_widget.insert("1.0", content)
+    # Buchstaben für "code" grün hervorheben
+    highlight_positions = {
+        'f': "1.35", #Indizes für die zu markierenden Buchstaben(Zeile und Stelle innerhalb der Zeile)
+        'r': "1.78",
+        'e1': "2.13",
+        'i1': "2.43",
+        'h': "5.25",
+        'e2': "5.113",
+        'i2': "6.19",
+        't': "6.134",
+    }
+    for char, pos in highlight_positions.items():
+        text_widget.tag_add(char, pos, f"{pos}+1c")
+        text_widget.tag_configure(char, foreground="green")
+    # Text-Widget nicht bearbeitbar machen
+    text_widget.configure(state="disabled")
 
     # Eingabefeld für das Lösungswort
     user_input = tk.Entry(cell)
     user_input.pack(pady=10)
-    eingabe = user_input.get()
-    print(eingabe)
 
-    # Prüfen-Schaltfläche
-    button_check = tk.Button(cell, text="Enter", command=check_password)
+    # Prüfen-Schaltfläche (verarbeitung)
+    button_check = tk.Button(cell, text="PRÜFEN", font='Helvetica 20', command=check_password, height=1, width=15, bg="black", fg="white")
     button_check.pack(pady=10)
+
+    # Ausgabefeld Schloss
+    text_output = tk.Text(cell, wrap="word", bg="white", fg="black", font=("Helvetica", 12, 'bold'))
+    text_output.pack(pady=10)
 
     # GUI starten
     cell.mainloop()
-#run()
+run()
--- Code/escape_room_main.py
+++ Code/escape_room_main.py
@@ -1,6 +1,6 @@
-#Hauptklasse von der alle anderen aufgerufen und verwaltet werden
+#Startbildschirm
 import tkinter as tk
-import Zelle
+import time
 
 # Startfenster erstellen
 root = tk.Tk()
@@ -17,15 +17,97 @@ Img = tk.PhotoImage(file="../images/prison.png")
 prisonBg = canvas.create_image(0,0,image=Img, anchor='nw')
 
 #Titeltext erstellen
-label = tk.Label(root, text="Prison-Escape", font=("Helvetica", 30, "bold"))
-label.place(relx=0.5, rely=0.4, anchor = 'center')
+label = tk.Label(root, text="Prison-Escape", bg="black", fg="white", font=("Helvetica", 30, "bold"))
+label.place(relx=0.5, rely=0.2, anchor = 'center')
 
-#Button -> Geht zum ersten Raum
-def start_cell():
+#Erster Raum - Gefängniszelle
+#------------start zelle----------------
+def run_zelle():
+    # Spielfenster erstellen
+    cell = tk.Tk()
+    cell.title("Zelle")
+    cell.geometry("800x800")
+    cell.resizable(width=False, height=False)
+
+    # Funktion zur Überprüfung des Lösungsworts (Verarbeitung der Eingabe)
+    def check_password():
+        eingabe = user_input.get().lower()
+        print(eingabe)
+        if eingabe == "freiheit":
+            output = "Glückwunsch! Die Tür öffnet sich."
+        else:
+            output = "Fehler!"
+
+    # Text Widget erstellen
+    text_widget = tk.Text(cell, wrap="word", bg="black", fg="white", font=("Helvetica", 10, "bold"))
+    #Text Widget platzieren
+    text_widget.pack(pady=10, fill="both", expand=False)
+    # Textinhalt setzen
+    content = ("Langsam öffnest du deine Augen und Findest dich alleine in einer kalten, düsteRen Gefängniszelle wieder.\n"
+    "Die AtmosphärE ist erdrückend, und die massIven Steinwände verstärken das Gefühl der Einsamkeit.\n"
+    "Du bemerkst, dass jemand eine Botschaft in die kahle Betonwand eingeritzt hat:\n\n"
+    "\"In der Stunde der Dunkelheit liegt der Schlüssel zum Entkommen verborgen. Ein mutiger SchRitt kann den ersten "
+    "Stein ins Rollen bringen. Öffne deine Augen und suche nach den verBorgenen Hinweisen.\n"
+    "Vertraue auf dEine Instinkte, und sei stets wachsam. Die WahRheit liegt nicht immer offensichtlich vor dir. "
+    "Beginne mit einem tiefen Atemzug, lausche dem Klang der Stille und folge dem Flüstern der Hoffnung.\"\n")
+    # Textinhalt dem Text-Widget hinzufügen
+    text_widget.insert("1.0", content)
+    # Buchstaben für "code" grün hervorheben
+    highlight_positions = {
+        'f': "1.35", #Indizes für die zu markierenden Buchstaben(Zeile und Stelle innerhalb der Zeile)
+        'r': "1.78",
+        'e1': "2.12",
+        'i1': "2.41",
+        'h': "3.42",
+        'e2': "4.8",
+        'i2': "4.99",
+        't': "5.33",
+    }
+    for char, pos in highlight_positions.items():
+        text_widget.tag_add(char, pos, f"{pos}+1c")
+        text_widget.tag_configure(char, foreground="green")
+    # Text-Widget nicht bearbeitbar machen
+    text_widget.configure(state="disabled")
+
+    # Eingabefeld für das Lösungswort
+    user_input = tk.Entry(cell)
+    user_input.pack(pady=10)
+
+    # Prüfen-Schaltfläche
+    button_check = tk.Button(cell, text="PRÜFEN", command=check_password)
+    button_check.pack(pady=10)
+
+    # Ausgabefeld Schloss
+    #label_output = tk.Label(cell, text=output)
+    #label_output.pack(pady=10)
+
+    # GUI starten
+    cell.mainloop()
+#------------ende zelle-------------
+
+#Funktion -> Geht zum ersten Raum
+def start_zelle():
     root.destroy()
-    Zelle.run()
-raum1 = tk.Button(root, text="Start", font='Helvetica 22', command=start_cell, height=2, width=12, bg="black", fg="white")
-raum1.place(relx = 0.5, rely = 0.55, anchor = 'center')
+    run_zelle()
+# Funktion -> Geht zum zweiten Raum
+def start_buero():
+    root.destroy()
+    #run_buero()
+# Funktion -> Geht zum dritten Raum
+def start_hof():
+    root.destroy()
+    #run_hof()
+def spielende():
+    root.destroy()
+#Buttons zum Starten in verschiedenen Räumen
+zelle = tk.Button(root, text="Start Zelle", font='Helvetica 22', command=start_zelle, height=1, width=15, bg="black", fg="white")
+zelle.place(relx = 0.5, rely = 0.4, anchor = 'center')
+buero = tk.Button(root, text="Start Wärterbüro", font='Helvetica 22', command=start_buero, height=1, width=15, bg="black", fg="white")
+buero.place(relx = 0.5, rely = 0.5, anchor = 'center')
+hof = tk.Button(root, text="Start Hof", font='Helvetica 22', command=start_hof, height=1, width=15, bg="black", fg="white")
+hof.place(relx = 0.5, rely = 0.6, anchor = 'center')
+beenden = tk.Button(root, text="Beenden", font='Helvetica 22', command=spielende, height=1, width=15, bg="black", fg="white")
+beenden.place(relx = 0.5, rely = 0.75, anchor = 'center')
 
 #GUI starten
 root.mainloop()
